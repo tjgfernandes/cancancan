8505f1031ed6a749be1491c20e07caebb4eb3a09
Tiago Fernandes <tjg.fernandes@gmail.com>
2016-03-02 00:00:44 +0000

    fix build_relation_with_unions inject init with empty relation parameter

--- lib/cancan/model_adapters/active_record_3_adapter.rb
+++ lib/cancan/model_adapters/active_record_3_adapter.rb
@@ -11,7 +11,7 @@ module CanCan
       def build_relation_with_unions(model_class,rules)
         rules.map do |rule|
           model_class.where(conditions_for_rule(rule)).joins(joins_for_rule(rule))
-        end.inject do |result, element|
+        end.inject(empty_relation(model_class)) do |result, element|
           result.union(element)
         end
       end
@@ -19,6 +19,12 @@ module CanCan
       def build_relation(*where_conditions)
         @model_class.where(*where_conditions).includes(joins)
       end
+
+      # used with inject to return emtpy relation instead of a nil value
+      def empty_relation(model_class)
+        model_class.where("1=1")
+      end
+
     end
   end
 end
